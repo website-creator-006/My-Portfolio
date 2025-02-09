76aa531002e47d696f0a5f65829857d9af7d2c5b
website-creator-006 <warriorforever006@gmail.com>
2025-02-09 17:50:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-function toggleMenu() {
-        document.getElementById("menu").classList.toggle("active");
-    }
-    
+document.addEventListener("DOMContentLoaded", function () {
+    const menuToggle = document.getElementById("mobile-menu");
+    const navLinks = document.querySelector(".nav-links");
+
+    menuToggle.addEventListener("click", () => {
+        navLinks.classList.toggle("show");
+    });
+});
